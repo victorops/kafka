25c6f7c309bc790257f9cc6a228004c70be40b44
charlie wanek <charlie@vaal.victorops.com>
2013-03-29 16:52:54 -0600

    add /etc/kafka to the classpath (for log4j)

--- bin/vo-kafka-run-class.sh
+++ bin/vo-kafka-run-class.sh
@@ -44,6 +44,8 @@ do
   CLASSPATH=$CLASSPATH:$file
 done
 
+CLASSPATH=$CLASSPATH:/etc/kafka
+
 if [ -z "$KAFKA_JMX_OPTS" ]; then
   KAFKA_JMX_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false  -Dcom.sun.management.jmxremote.ssl=false "
 fi
